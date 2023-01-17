e2556861f5b678a5410938679b08681b36fd5eb1
Rodney Gauna <rodneygauna@gmail.com>
2023-01-16 23:23:31 -0800

    user linked to department

--- src/departments/views.py
+++ src/departments/views.py
@@ -3,8 +3,8 @@ Department views for emberstone
 '''
 
 # Imports
-from flask import Blueprint, render_template, redirect, url_for
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect, url_for, make_response
+from bson.objectid import ObjectId
 from src.departments.forms import DepartmentForm
 from src import db
 
@@ -33,21 +33,28 @@ def add_department():
                                    # TODO: Fix this message
                                    error='Error: Department already exists')
         else:
-            departments.insert_one({'nfirs_id': form.nfirs_id.data,
-                                    'name': form.name.data,
-                                    'street_number': form.street_number.data,
-                                    'street_prefix': form.street_prefix.data,
-                                    'street_name': form.street_name.data,
-                                    'street_type': form.street_type.data,
-                                    'street_suffix': form.street_suffix.data,
-                                    'city': form.city.data,
-                                    'state': form.state.data,
-                                    'zipcode': form.zipcode.data,
-                                    'county_code': form.county_code.data,
-                                    'tele_phone': form.tele_phone.data,
-                                    'fax_phone': form.fax_phone.data,
-                                    'status': form.status.data})
-            return redirect(url_for('users.register'))
+            new_department = departments.insert_one(
+                {'_id': ObjectId(),
+                 'nfirs_id': form.nfirs_id.data,
+                 'name': form.name.data,
+                 'street_number': form.street_number.data,
+                 'street_prefix': form.street_prefix.data,
+                 'street_name': form.street_name.data,
+                 'street_type': form.street_type.data,
+                 'street_suffix': form.street_suffix.data,
+                 'city': form.city.data,
+                 'state': form.state.data,
+                 'zipcode': form.zipcode.data,
+                 'county_code': form.county_code.data,
+                 'tele_phone': form.tele_phone.data,
+                 'fax_phone': form.fax_phone.data,
+                 'status': form.status.data,
+                 'users': []})
+            # Store department in a cookie
+            resp = make_response(redirect(url_for('users.register')))
+            resp.set_cookie('new_department', str(new_department.inserted_id))
+            # Redirect user to register page with department cookie
+            return resp
     return render_template('add_department.html',
                            title='Emberstone - Add Department',
                            form=form)
--- src/users/views.py
+++ src/users/views.py
@@ -3,9 +3,11 @@ User views for emberstone
 '''
 
 # Imports
-from flask import Blueprint, render_template, redirect, url_for
-from flask_login import login_user, logout_user, login_required
+from flask import Blueprint, render_template, redirect, url_for, request
+from flask import make_response
+from flask_login import login_user, logout_user, login_required, current_user
 import bcrypt
+from bson.objectid import ObjectId
 from src.users.forms import LoginForm, RegisterForm
 from src.models import User
 from src import db
@@ -17,6 +19,7 @@ users_bp = Blueprint('users', __name__)
 
 # Database - Users variable
 users = db.users
+departments = db.departments
 
 
 # Routes
@@ -37,9 +40,13 @@ def login():
 
         if user and bcrypt.checkpw(form.password.data.encode('utf-8'),
                                    user['password']):
-            user_obj = User(user['email'], user['password'])
+            user_obj = User(_id=str(user["_id"]),
+                            email=user["email"],
+                            password=user["password"],
+                            departments=user["departments"])
+            login_user(user_obj)
             # remember user
-            login_user(user_obj, remember=form.remember.data)
+            login_user(user_obj)
 
             # Redirect user to home page
             return redirect(url_for('core.index'))
@@ -54,8 +61,15 @@ def login():
 @login_required
 def logout():
     '''Route: Logout Page'''
-    # Log user out
-    logout_user()
+    user = current_user
+    if isinstance(user, User):
+        logout_user()
+    else:
+        user_obj = User(_id=str(user["_id"]),
+                        email=user["email"],
+                        password=user["password"],
+                        departments=user["departments"])
+        logout_user()
 
     # Redirect user to login page
     return redirect(url_for('users.login'))
@@ -67,18 +81,36 @@ def register():
     '''Route: User Registration Page'''
     form = RegisterForm()
     if form.validate_on_submit():
+        # Check if user exists
         existing_user = users.find_one({'email': form.email.data})
         if existing_user:
             return render_template('register.html',
                                    form=form,
                                    # TODO: Fix this message
                                    error='Error: Email already exists.')
+        # If user doesn't exist, create user
         else:
+            # Hash password
             hashed_password = bcrypt.hashpw(
                 form.password.data.encode('utf-8'), bcrypt.gensalt())
-            users.insert_one({'email': form.email.data,
-                              'password': hashed_password})
-            return redirect(url_for('users.login'))
+            # Insert user into database
+            users.insert_one({'_id': ObjectId(),
+                              'email': form.email.data,
+                              'password': hashed_password,
+                              'departments': []})
+            # Check if user is being created from a department
+            new_department = request.cookies.get('new_department')
+            if new_department:
+                departments.update_one({'_id': ObjectId(new_department)},
+                                       {'$push': {'users': form.email.data}})
+                users.update_one({'email': form.email.data},
+                                 {'$push': {'departments':
+                                            ObjectId(new_department)}})
+                # Remove department cookie
+                resp = make_response(redirect(url_for('users.login')))
+                resp.delete_cookie('new_department')
+                # Redirect user to login page and delete department cookie
+                return resp
     return render_template('register.html',
                            title='Emberstone - Register',
                            form=form)
