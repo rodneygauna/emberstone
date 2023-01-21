2fce87e9a3e1d3fcc083f023ff4e49ef3c2af3a5
Rodney Gauna <rodneygauna@gmail.com>
2023-01-21 14:34:28 -0800

    addition of select2 to large dropdowns

---
+++ src/static/js/main.js
@@ -0,0 +1,9 @@
+// Select2 plugin
+// -------------------
+// This is a custom plugin for select2
+// It is used to add a search box to the select2 dropdown
+// It is used in the select2 dropdowns in the form advanced plugin
+$(document).ready(function() {
+  // Add search box to select2 dropdown
+  $('.select2-search').select2();
+});
