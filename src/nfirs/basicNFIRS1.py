cb6a901256690602d9d8192d9710e05aa955160a
Rodney Gauna <rodneygauna@gmail.com>
2023-01-25 13:48:10 -0800

    sections A-E completed

--- src/nfirs/basicNFIRS1.py
+++ src/nfirs/basicNFIRS1.py
@@ -4,11 +4,12 @@ Basic Module (NFIRS-1) form
 
 # Imports
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, IntegerField
-from wtforms import DateField
+from wtforms import StringField, SubmitField, SelectField, IntegerField, \
+    DateField, TimeField
 from wtforms.validators import DataRequired, InputRequired, Length
 from src.dictionaries import INDICENT_REPORTING_STATUS, LOCATION
 from src.dictionaries import STREET_PREFIX_SUFFIX, STREET_TYPE_CHOICES, STATE
+from src.dictionaries import INCIDENT_TYPE, AID_GIVEN
 
 
 # Form - Basic Module (NFIRS-1)
@@ -47,6 +48,52 @@ class BasicModuleForm(FlaskForm):
     city = StringField('City', validators=[Length(max=50)])
     state = SelectField('State', choices=STATE)
     zipcode = StringField('ZIP Code', validators=[Length(min=5, max=9)])
+    crossstreet_directions_usnationalgrid = StringField(
+        'Cross Street, Directions, or US National Grid',
+        validators=[Length(max=30)])
+
+    # Section C - Incident Type
+    incident_type = SelectField('Incident Type', choices=INCIDENT_TYPE)
+
+    # Section D - Aid Given or Received
+    aid_given_or_received = SelectField(
+        'Aid Given or Received', choices=AID_GIVEN,
+        validators=[DataRequired()])
+    fdid_receiving_aid = StringField(
+        'FDID Receiving Aid', validators=[Length(min=5, max=5)])
+    state_receiving_aid = SelectField('State Receiving Aid', choices=STATE)
+    incident_number_receiving_aid = IntegerField(
+        'Incident Number Receiving Aid', validators=[Length(max=7)])
+
+    # Section E1 - Dates and Times
+    alarm_date = DateField('Alarm Date', validators=[InputRequired()])
+    alarm_time = TimeField('Alarm Time', validators=[InputRequired()])
+    arrival_date = DateField('Arrival Date', validators=[InputRequired()])
+    arrival_time = TimeField('Arrival Time', validators=[InputRequired()])
+    controlled_date = DateField('Controlled Date')
+    controlled_time = TimeField('Controlled Time')
+    cleared_date = DateField('Cleared Date')
+    cleared_time = TimeField('Cleared Time')
+
+    # Section E2 - Shifts and Alarms
+    shift_or_platoon = StringField(
+        'Shift or Platoon', validators=[Length(max=1)])
+    alarms = StringField('Alarms', validators=[Length(max=2)])
+    district = StringField('District', validators=[Length(max=3)])
+
+    # Section E3 - Special Studies
+    special_study_sequence_number_1 = StringField(
+        'Special Study Sequence Number #1', validators=[Length(max=3)])
+    special_study_id_1 = StringField(
+        'Special Study ID #1', validators=[Length(max=5)])
+    special_study_code_1 = StringField(
+        'Special Study Code #1', validators=[Length(max=5)])
+    special_study_sequence_number_2 = StringField(
+        'Special Study Sequence Number #2', validators=[Length(max=3)])
+    special_study_id_2 = StringField(
+        'Special Study ID #2', validators=[Length(max=5)])
+    special_study_code_2 = StringField(
+        'Special Study Code #2', validators=[Length(max=5)])
 
     # Submit
     submit = SubmitField('Save')
