54c3b28d7d722c5618ed49ed60b842f80d199fd8
Rodney Gauna <rodneygauna@gmail.com>
2023-08-12 22:44:27 +0000

    dictionaries added

---
+++ src/dictionaries/dict_general.py
@@ -0,0 +1,11 @@
+"""
+Dictionaries > dict_county.py
+General dictionaries.
+"""
+
+
+# Status Dictionary
+STATUS = [
+    ('Active', 'Active'),
+    ('Inactive', 'Inactive')
+]
---
+++ src/dictionaries/dict_names.py
@@ -0,0 +1,31 @@
+"""
+Dictionaries > dict_names.py
+Dictionaries for names.
+
+NFIRS Data dictionary can be found here:
+https://www.usfa.fema.gov/downloads/xls/NFIRS_Spec_Tables_2013.xls
+"""
+
+# Name Prefix Dictionary
+NAME_PREFIX = [
+    ('', ''),
+    ('MR', 'Mr.'),
+    ('MRS', 'Mrs.'),
+    ('MS', 'Ms.'),
+    ('DR', 'Doctor'),
+    ('REV', 'Reverend')
+]
+
+# Name Suffix Dictionary
+NAME_SUFFIX = [
+    ('', ''),
+    ('JR', 'Junior'),
+    ('SR', 'Senior'),
+    ('I', 'The First'),
+    ('II', 'The Second'),
+    ('III', 'The Third'),
+    ('IV', 'The Fourth'),
+    ('V', 'The Fifth'),
+    ('MD', 'Medical Doctor'),
+    ('DDS', 'Doctor of Dental Science')
+]
