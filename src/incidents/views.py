4f41ab893b37593b31e1eaba4ffe553d70c0ad7d
Rodney Gauna <rodneygauna@gmail.com>
2023-11-05 17:01:05 -0800

    start of the nfirs1 form add/edit

--- src/incidents/views.py
+++ src/incidents/views.py
@@ -13,6 +13,8 @@ from flask_login import (
     login_required, current_user)
 from sqlalchemy import func
 from src.nfirsforms.nfirs1basic import BasicModuleForm
+from src import db
+from src.models import NFIRS1Basic
 
 
 # Blueprint variable
@@ -29,3 +31,45 @@ def incidents():
         'incidents/incidents.html',
         title='Incidents'
     )
+
+
+# Route - Incidents > New Basic (NFIRS 1)
+@login_required
+@incidents_bp.route('/incidents/new/basic',
+                    methods=['GET', 'POST'])
+def nfirs1():
+    """Basic Incident Information (NFIRS 1)"""
+
+    form = BasicModuleForm()
+
+    if form.validate_on_submit():
+        # Create Incident
+        new_incident = NFIRS1Basic(
+            # Section A - Incident Header
+            # Section B - Location
+            # Section C - Incident Type
+            # Section D - Aid Given or Received
+            # Section E1 - Date and Times
+            # Section E2 - Shifts and Alarms
+            # Section E3 - Special Studies
+            # Section F - Actions Taken
+            # Section G1 - Resources
+            # Section G2 - Estimated Dollar Losses and Values
+            # Section H - Completed Models
+            # Section H1 - Casualties
+            # Section H2 - Detector
+            # Section H3 - Hazardous materials Release
+            # Section I - Mixed Use
+            # Section J - Property Use
+            # Section K1 - Person/Entity Involved
+            # Section K2 - Owner
+            # Section L - Remarks
+            # Section M - Authorization
+        )
+        # Add Incident to database
+        db.session.add(new_incident)
+        db.session.commit()
+        # Flash success message
+        flash('Incident created successfully!', 'success')
+        # Redirect to Incidents Landing Page
+        return redirect(url_for('incidents.incidents'))
