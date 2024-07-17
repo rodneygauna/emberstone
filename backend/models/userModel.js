249c19fe5ded34d4bb997f14fc79f8d008b2f60a
Rodney Gauna <rodneygauna@gmail.com>
2024-07-17 14:39:48 -0700

    more improvements

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,26 +1,42 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema ({
-  firstname: {
-    type: String,
-    required: [true, "First name is required"]
+const userSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: [true, "First name is required"],
+    },
+    last_name: {
+      type: String,
+      required: [true, "Last name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    password_hash: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    user_role: {
+      type: String,
+      enum: {
+        values: ["user", "super user", "admin"],
+        message:
+          "{VALUE} is not supported; only user, super user, or admin are allowed.",
+      },
+      default: "user",
+    },
   },
-  lastname: {
-    type: String,
-    required: [true, "Last name is required"]
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: [true, "Email must be unique"]
-  },
-  password_hash: {
-    type: String,
-    required: [true, "Password is required"]
-  }
-},
   {
     timestamps: true,
-  })
+  }
+);
 
 export default mongoose.model("User", userSchema);
