21be63126ed34e94df57c900255c32518074f3ac
Rodney Gauna <rodneygauna@gmail.com>
2024-07-17 21:14:19 -0700

    added mongo options

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,20 @@ import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
+// MongoDB Options
+const options = {
+  useNewUrlParser: true,
+  autoIndex: false,
+  maxPoolSize: 10,
+};
+
+
 // Start server and connect to MongoDB
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, options);
     console.log(`Connected to MongoDB`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
