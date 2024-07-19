62a789732fdcd7be1d71af5eec023b272e654bb8
Rodney Gauna <rodneygauna@gmail.com>
2024-07-19 03:12:33 -0700

    docker compose improvements

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,19 @@ import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
-// MongoDB Options
-const options = {
-  useNewUrlParser: true,
-  autoIndex: false,
-  maxPoolSize: 10,
-};
+// Get environment variables
+const { MONGO_HOST, MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE } =
+  process.env;
 
+// MongoDB connection string
+const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?authSource=admin`;
 
 // Start server and connect to MongoDB
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
   try {
-    await mongoose.connect(process.env.MONGO_URI, options);
+    await mongoose.connect(MONGO_URI);
     console.log(`Connected to MongoDB`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
