8c4449bc0a010bba3d141f46db0cbabd2e473a9d
Rodney Gauna <rodneygauna@gmail.com>
2024-07-26 07:52:34 -0700

    starting over

--- backend/models/basic/cIncidentTypeModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import { requiredEnum } from "../../utils/validation/validationConstants.js";
-import incidentTypeCodes from "../../utils/enumValues/incidentTypeCodes.js";
-
-const cIncidentTypeSchema = new mongoose.Schema({
-  incident_type: requiredEnum("Incident Type", incidentTypeCodes),
-});
-
-export default mongoose.model("incidentType", cIncidentTypeSchema);
