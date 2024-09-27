227cf431107c267ad8a613a0ae32e9e45a86837c
Rodney Gauna <rodneygauna@gmail.com>
2024-09-27 11:53:20 -0700

    refactor: Update import statements and add routing in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,32 @@
-import './assets/css/normalize.css'
-import './assets/css/skeleton.css'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
+// Import CSS
+import "./assets/css/normalize.css";
+import "./assets/css/skeleton.css";
+
+// Import Page Layouts
+import MainLayout from "./layouts/MainLayout";
+
+// Pages - Home Page
+import HomePage from "./pages/HomePage";
+
+// App
 function App() {
-  return (
-    <>
-      <h1>Emberstone</h1>
-    </>
-  )
+  // Routes for pages
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
