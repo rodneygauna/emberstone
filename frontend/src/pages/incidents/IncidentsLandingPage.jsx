d668b820a693b2563c71755a6fc94d457acbfa58
Rodney Gauna <rodneygauna@gmail.com>
2024-10-02 10:24:25 -0700

    feat: Add IncidentsLandingPage component with LeftNav

---
+++ frontend/src/pages/incidents/IncidentsLandingPage.jsx
@@ -0,0 +1,11 @@
+import LeftNav from "../../components/global/LeftNav";
+
+const IncidentsLandingPage = () => {
+  return (
+    <div>
+      <LeftNav />
+    </div>
+  );
+};
+
+export default IncidentsLandingPage;
