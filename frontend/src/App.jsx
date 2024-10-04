c24c5a94766e6af22cb50c6991595e1973d15c7f
Rodney Gauna <rodneygauna@gmail.com>
2024-10-04 12:35:39 -0700

    refactor: Update App.jsx to add CreateDepartmentPage component and related hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,26 +18,40 @@ import LoginPage from "./pages/auth/LoginPage";
 import RegistrationPage from "./pages/auth/RegistrationPage";
 // Pages - Incidents
 import IncidentsLandingPage from "./pages/incidents/IncidentsLandingPage";
+// Pages - Settings - Department
+import CreateDepartmentPage from "./pages/department/CreateDepartmentPage";
 
 // Hooks - Auth
 import useUserAuth from "./hooks/auth/userAuth";
 import useUserRegister from "./hooks/auth/userRegister";
+// Hooks - Settings - Department
+import useDepartmentAdd from "./hooks/department/departmentAdd";
 
 // App
 function App() {
+  // Auth hooks
   const { loginUser } = useUserAuth();
   const { registerUser } = useUserRegister();
+  // Settings - Department hooks
+  const { addDepartment } = useDepartmentAdd();
 
   // Routes for pages
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
+        {/*Routes - Auth*/}
         <Route index element={<LoginPage userLoginSubmit={loginUser} />} />
         <Route
           path="/register"
           element={<RegistrationPage userRegistrationSubmit={registerUser} />}
         />
+        {/*Routes - Incidents*/}
         <Route path="/incidents" element={<IncidentsLandingPage />} />
+        {/*Routes - Settings - Department*/}
+        <Route
+          path="/settings/department/add"
+          element={<CreateDepartmentPage departmentAddSubmit={addDepartment} />}
+        />
       </Route>
     )
   );
