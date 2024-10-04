3af6a733c4519a1d4e54928787db765c9bc8b70b
Rodney Gauna <rodneygauna@gmail.com>
2024-10-04 12:35:25 -0700

    feat: Add CreateDepartmentPage component with form and navigation
    This commit adds the CreateDepartmentPage component to the application. The component includes a form for creating a new department and navigation using the LeftNav component. The form includes input fields for various department details such as NFIRS ID, State FDID, Fire Department Name, and more. Upon form submission, the data is filtered and passed to the departmentAddSubmit function. If successful, the user is navigated to the department page and a success toast message is displayed. Any errors during the submission process are displayed as error toast messages.

---
+++ frontend/src/pages/department/CreateDepartmentPage.jsx
@@ -0,0 +1,288 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import PropTypes from "prop-types";
+
+import LeftNav from "../../components/global/LeftNav";
+
+const CreateDepartmentPage = ({ departmentAddSubmit }) => {
+  // Form state
+  const [formData, setFormData] = useState({
+    nfirs_id: "",
+    state_fdid: "",
+    name: "",
+    street_number_or_milepost: "",
+    street_prefix: "",
+    street_or_highway_name: "",
+    street_type: "",
+    street_suffix: "",
+    city: "",
+    state: "",
+    zip: "",
+    county_code: "",
+    phone_number: "",
+    fax_number: "",
+  });
+
+  // Form change handler
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  // Navigation
+  const navigate = useNavigate();
+
+  // Form submit handler
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    // filter out empty strings
+    const filteredData = Object.fromEntries(
+      Object.entries(formData).filter(([, value]) => value.trim() !== "")
+    );
+
+    departmentAddSubmit(filteredData)
+      .then(() => {
+        navigate("/department");
+        toast.success("Department added successfully");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
+  // HTML form
+  return (
+    <>
+      <div className="flex-container">
+        <LeftNav />
+        <div className="right-panel-container container">
+          <form onSubmit={submitForm}>
+            <h2>Create Department</h2>
+            <div>
+              <label className="u-pull-left" htmlFor="nfirs_id">
+                NFIRS ID
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="nfirs_id"
+                name="nfirs_id"
+                placeholder="123456789"
+                value={formData.nfirs_id}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="state_fdid">
+                State FDID
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="state_fdid"
+                name="state_fdid"
+                placeholder="123456789"
+                value={formData.state_fdid}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="name">
+                Fire Department Name
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="name"
+                name="name"
+                placeholder="City Fire Department"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label
+                className="u-pull-left"
+                htmlFor="street_number_or_milepost"
+              >
+                Street Number or Milepost
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="street_number_or_milepost"
+                name="street_number_or_milepost"
+                placeholder="123"
+                value={formData.street_number_or_milepost}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="street_prefix">
+                Street Prefix
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="street_prefix"
+                name="street_prefix"
+                placeholder="N"
+                value={formData.street_prefix}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="street_or_highway_name">
+                Street or Highway Name
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="street_or_highway_name"
+                name="street_or_highway_name"
+                placeholder="Main"
+                value={formData.street_or_highway_name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="street_type">
+                Street Type
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="street_type"
+                name="street_type"
+                placeholder="St"
+                value={formData.street_type}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="street_suffix">
+                Street Suffix
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="street_suffix"
+                name="street_suffix"
+                placeholder="N"
+                value={formData.street_suffix}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="city">
+                City
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="city"
+                name="city"
+                placeholder="Anytown"
+                value={formData.city}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="state">
+                State
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="state"
+                name="state"
+                placeholder="CA"
+                value={formData.state}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="zip">
+                Zip
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="zip"
+                name="zip"
+                placeholder="12345"
+                value={formData.zip}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="county_code">
+                County Code
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="county_code"
+                name="county_code"
+                placeholder="6073"
+                value={formData.county_code}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="phone_number">
+                Phone Number
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="phone_number"
+                name="phone_number"
+                placeholder="5556661234"
+                value={formData.phone_number}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="u-pull-left" htmlFor="fax_number">
+                Fax Number
+              </label>
+              <input
+                className="u-full-width"
+                type="text"
+                id="fax_number"
+                name="fax_number"
+                placeholder="5556661234"
+                value={formData.fax_number}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+CreateDepartmentPage.propTypes = {
+  departmentAddSubmit: PropTypes.func.isRequired,
+};
+
+export default CreateDepartmentPage;
