f6dee938b71d0c3a6d30871a3f43f26487cbb43d
Rodney Gauna <rodneygauna@gmail.com>
2024-10-14 10:01:25 -0700

    refactor: Update useAuthRedirect and useLandingRedirect hooks to include toast notifications

--- frontend/src/hooks/auth/useAuthRedirect.jsx
+++ frontend/src/hooks/auth/useAuthRedirect.jsx
@@ -1,6 +1,7 @@
 // Redirect to login page if user is not logged in (has no token)
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const useAuthRedirect = () => {
   const navigate = useNavigate();
@@ -8,6 +9,7 @@ const useAuthRedirect = () => {
   useEffect(() => {
     if (!localStorage.getItem("token")) {
       navigate("/");
+      toast.error("You must be logged in to view this page");
     }
   }, [navigate]);
 };
--- frontend/src/hooks/auth/useLandingRedirect.jsx
+++ frontend/src/hooks/auth/useLandingRedirect.jsx
@@ -1,6 +1,7 @@
 // Redirect to the app's landing page if the user is logged in (has a token)
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const useLandingRedirect = () => {
   const navigate = useNavigate();
@@ -8,6 +9,7 @@ const useLandingRedirect = () => {
   useEffect(() => {
     if (localStorage.getItem("token")) {
       navigate("/incidents");
+      toast.info("You are already logged in");
     }
   }, [navigate]);
 };
