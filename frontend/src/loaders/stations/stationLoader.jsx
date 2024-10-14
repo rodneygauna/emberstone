453959e199671a1d49bb8ac606967b1981539c47
Rodney Gauna <rodneygauna@gmail.com>
2024-10-14 09:57:49 -0700

    refactor: Refactored departmentLoader and stationLoader to include authentication check

--- frontend/src/loaders/stations/stationLoader.jsx
+++ frontend/src/loaders/stations/stationLoader.jsx
@@ -1,5 +1,13 @@
-// Station Loader - fetches station data from the API
+import { checkAuth } from "../../utils/auth/checkAuth";
+
+// Desc: Station Loader - fetches station data from the API
 export const stationLoader = async ({ params }) => {
+  // If the user is not logged in, return an error
+  const authCheck = checkAuth();
+  if (authCheck) {
+    return authCheck;
+  }
+
   const response = await fetch(`/api/v1/stations/${params.id}`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
