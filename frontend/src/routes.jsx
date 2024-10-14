4e5ba7dddc0694239ed54c1a8e1ac91c106eda7e
Rodney Gauna <rodneygauna@gmail.com>
2024-10-14 09:16:05 -0700

    refactor: Update routes.jsx to use departmentLoader from loaders

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -12,39 +12,40 @@ import EditDepartmentPage from "./pages/department/EditDepartmentPage";
 // Pages - Settings - Stations
 import CreateStationPage from "./pages/stations/CreateStationPage";
 
-// Loaders
-import { departmentLoader } from "./loaders/Loaders";
-
-const createRoutes = (hooks) => (
-  <Route path="/" element={<MainLayout />}>
-    {/* Routes - Auth */}
-    <Route index element={<LoginPage userLoginSubmit={hooks.loginUser} />} />
-    <Route
-      path="/register"
-      element={<RegistrationPage userRegistrationSubmit={hooks.registerUser} />}
-    />
-    {/* Routes - Incidents */}
-    <Route path="/incidents" element={<IncidentsLandingPage />} />
-    {/* Routes - Settings - Department */}
-    <Route
-      path="/settings/department/add"
-      element={
-        <CreateDepartmentPage departmentAddSubmit={hooks.addDepartment} />
-      }
-    />
-    <Route
-      path="/settings/department/edit/:id"
-      element={
-        <EditDepartmentPage departmentEditSubmit={hooks.editDepartment} />
-      }
-      loader={departmentLoader}
-    />
-    {/* Routes - Settings - Stations */}
-    <Route
-      path="/settings/stations/add"
-      element={<CreateStationPage stationAddSubmit={hooks.addStation} />}
-    />
-  </Route>
-);
+const createRoutes = (hooks, loaders) => {
+  return (
+    <Route path="/" element={<MainLayout />}>
+      {/* Routes - Auth */}
+      <Route index element={<LoginPage userLoginSubmit={hooks.loginUser} />} />
+      <Route
+        path="/register"
+        element={
+          <RegistrationPage userRegistrationSubmit={hooks.registerUser} />
+        }
+      />
+      {/* Routes - Incidents */}
+      <Route path="/incidents" element={<IncidentsLandingPage />} />
+      {/* Routes - Settings - Department */}
+      <Route
+        path="/settings/department/add"
+        element={
+          <CreateDepartmentPage departmentAddSubmit={hooks.addDepartment} />
+        }
+      />
+      <Route
+        path="/settings/department/edit/:id"
+        element={
+          <EditDepartmentPage departmentEditSubmit={hooks.editDepartment} />
+        }
+        loader={loaders.departmentLoader}
+      />
+      {/* Routes - Settings - Stations */}
+      <Route
+        path="/settings/stations/add"
+        element={<CreateStationPage stationAddSubmit={hooks.addStation} />}
+      />
+    </Route>
+  );
+};
 
 export default createRoutes;
