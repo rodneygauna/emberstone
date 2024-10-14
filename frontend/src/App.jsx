d09952301eec974794a0b3c59f65584df77e7c7b
Rodney Gauna <rodneygauna@gmail.com>
2024-10-14 09:16:11 -0700

    refactor: Update App.jsx to use useAppLoaders hook in createRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,12 +12,14 @@ import "./assets/css/custom.css";
 // Routes and Hooks
 import createRoutes from "./routes";
 import useAppHooks from "./hooks";
+import useAppLoaders from "./loaders";
 
 function App() {
   const hooks = useAppHooks();
+  const loaders = useAppLoaders();
 
   const router = createBrowserRouter(
-    createRoutesFromElements(createRoutes(hooks))
+    createRoutesFromElements(createRoutes(hooks, loaders))
   );
 
   return <RouterProvider router={router} />;
